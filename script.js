69c490e2026ddb894f7003021910af94aa38f1af
Lucas00Lima <117579221+Lucas00Lima@users.noreply.github.com>
2023-01-18 17:18:13 -0300

    Remoção
    Removido os caracteres especiais

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let sizePassword = document.querySelector("#valor")
 let password = document.querySelector("#password")
 let containerPassword =document.querySelector("#container-password")
 
-let charset ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&"
+let charset ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 let novaSenha = ""
 
 sizePassword.innerHTML = sliderElement.value
